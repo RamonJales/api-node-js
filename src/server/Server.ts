5508a379c9a4eb498dd0c1d8fd65d0725d6ec935
RamonJales <ramonjales123@gmail.com>
2024-01-17 20:29:36 -0300

    eslint and editorconfig added

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import express from "express";
 
 const server = express(); //create an instance of our server
 
-server.get('/', (req, res) => {return res.send('Hello!');});
+server.get("/", (req, res) => {return res.send("Hello!");});
 
 export{server}; //export the server
