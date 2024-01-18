8a58a863d96da7bdec423b055c9abd73c5c3ea65
RamonJales <ramonjales123@gmail.com>
2024-01-17 23:29:12 -0300

    tells the application to use json

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -4,6 +4,8 @@ import {router} from "./routes";
 
 const server = express(); //create an instance of our server
 
+server.use(express.json()); // tells the application to use json
+
 server.use(router); //using the methods difined in routes/index.ts file
 
 export{server}; //export the server
