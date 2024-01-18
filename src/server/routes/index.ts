44cf59d9def0be9d4f3cf48fa286ba5f88479436
RamonJales <ramonjales123@gmail.com>
2024-01-17 23:42:28 -0300

    add post method and statusCode library

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,6 +2,11 @@ import {Router} from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {return res.send("Hello!");});
+router.get("/api", (req, res) => {return res.send("Hello!");});
+
+router.post("/api", (req, res) => {
+	console.log(req.body);
+	return res.json(req.body);
+});
 
 export {router};
