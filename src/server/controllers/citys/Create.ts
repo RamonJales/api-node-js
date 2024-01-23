f5dd753a96bcfd97ae36768b5c4b460fd2d7536b
RamonJales <ramonjales123@gmail.com>
2024-01-22 22:05:30 -0300

    add yup validation

--- src/server/controllers/citys/Create.ts
+++ src/server/controllers/citys/Create.ts
@@ -1,10 +1,28 @@
 import { Request, Response } from "express";
+import * as yup from "yup";
 
 interface city {
 	name: string;
 }
 
-export const create = (req: Request<{}, {}, city>, res: Response) => {
-	console.log(req.body);
+const bodyValidation: yup.ObjectSchema<city> = yup.object().shape({
+	name: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, city>, res: Response) => {
+	let validatedData: city | undefined = undefined;
+	
+	try {
+		validatedData = await bodyValidation.validate(req.body);
+	} catch (error) {
+		const yupError = error as yup.ValidationError;
+		return res.json({
+			errors: {
+				default: yupError.message,
+			}
+		});
+	}
+	
+	console.log(validatedData);
 	return res.send("Created!");
 };
